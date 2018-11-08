efa8f530e28dc0a0d4bfb8156d76978c084ef9f5
Administrator <administrator@iMac.local>
2018-11-08 14:28:35 +0200

    add production

--- index.js
+++ index.js
@@ -16,6 +16,17 @@ require('./start-up/mongo')();
 
 app.use('/api/post', post);
 
+
+if(process.env.NODE_ENV === 'production') {
+
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+
+}
+
+
 app.listen(5000, () => {
     console.log('Приклад застосунку, який прослуховує 5000-ий порт!');
 });
